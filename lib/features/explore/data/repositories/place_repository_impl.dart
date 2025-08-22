83dc09328a2d2f04dbb398a5fb6d1cdfafc1c856
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-08-22 20:00:54 +0900

    [fix] 기존 탐색 관련 코드 주석화

--- lib/features/explore/data/repositories/place_repository_impl.dart
+++ lib/features/explore/data/repositories/place_repository_impl.dart
@@ -1,13 +1,18 @@
-import 'package:heat_trip_flutter/features/explore/data/sources/place_api.dart';
-import 'package:heat_trip_flutter/features/explore/domain/entities/place_item.dart';
-import 'package:heat_trip_flutter/features/explore/domain/repositories/place_repository.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:heat_trip_flutter/features/explore/data/sources/place_api.dart';
+// import 'package:heat_trip_flutter/features/explore/domain/entities/place_item.dart';
+// import 'package:heat_trip_flutter/features/explore/domain/repositories/place_repository.dart';
 
-class PlaceRepositoryImpl implements PlaceRepository {
-  final PlaceApi remote;
-  PlaceRepositoryImpl(this.remote);
+// class PlaceRepositoryImpl implements PlaceRepository {
+//   final PlaceApi remote;
+//   PlaceRepositoryImpl(this.remote);
 
-  @override
-  Future<List<PlaceItem>> getPlaceItems({String? category}) {
-    return remote.fetchPlaceItems(category: category);
-  }
-}
+//   final String baseUrl = dotenv.env['API_BASE_URL'] ?? '';
+
+//   // 현재 페이지 정보 주고 페이지네이션 값 받아오기
+
+//   @override
+//   Future<List<PlaceItem>> getPlaceItems({String? category}) {
+//     return remote.fetchPlaceItems(category: category);
+//   }
+// }
--- lib/features/explore/domain/usecases/get_place_items.dart
+++ lib/features/explore/domain/usecases/get_place_items.dart
@@ -1,11 +1,11 @@
-import 'package:heat_trip_flutter/features/explore/domain/entities/place_item.dart';
-import 'package:heat_trip_flutter/features/explore/domain/repositories/place_repository.dart';
+// import 'package:heat_trip_flutter/features/explore/domain/entities/place_item.dart';
+// import 'package:heat_trip_flutter/features/explore/domain/repositories/place_repository.dart';
 
-class GetPlaceItems {
-  final PlaceRepository repo;
-  GetPlaceItems(this.repo);
+// class GetPlaceItems {
+//   final PlaceRepository repo;
+//   GetPlaceItems(this.repo);
 
-  Future<List<PlaceItem>> call({String? category}) {
-    return repo.getPlaceItems(category: category);
-  }
-}
+//   Future<List<PlaceItem>> call({String? category}) {
+//     return repo.getPlaceItems(category: category);
+//   }
+// }
