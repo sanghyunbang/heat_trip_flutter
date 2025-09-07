72802f0650d4c8b57f4308256360497ea9b48fc1
HYUN <149238724+sanghyunbang@users.noreply.github.com>
2025-09-07 18:04:32 +0900

    [build] android 기본 세팅 관련
    - 패키지 명 변경
    - 빌드 시 JAVA 버전 11 -> 17

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,19 +1,16 @@
-// allprojects {
-//     repositories {
-//         google()
-//         mavenCentral()
-//     }
-// }
-
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+// android/build.gradle.kts  (root)
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
-subprojects {
-    project.evaluationDependsOn(":app")
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 tasks.register<Delete>("clean") {
